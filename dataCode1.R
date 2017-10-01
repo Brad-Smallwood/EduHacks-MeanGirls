6e9688b0ae9699ec075ff8e0f849c54ee2caf643
Barinder Thind <b.thindd@gmail.com>
2017-09-30 23:46:08 -0700

    data sets updated lol

--- dataCode1.R
+++ dataCode1.R
@@ -16,22 +16,38 @@ library(dplyr)
 library(osmar)
 library(igraph)
 
-?OpenStreetMap()
-?get_osm()
-?osmsource_api()
-
 api <- osmsource_api(url = "http://api.openstreetmap.org/api/0.6/")
 
 box <- corner_bbox(-122.7629, 49.1860, -122.7392, 49.1979)
 
-gschw <- get_osm(box, source = api)
-gschw$nodes
-gschw$relations
+area_data1 <- get_osm(box, source = api)
+area_data1$nodes
+area_data1$relations
 
-str(gschw$nodes)
-View(gschw$nodes)
-View(gschw$nodes[2])
-View(gschw$nodes[1])
+node1 <- as.data.frame(area_data1$nodes[1])
+node2 <- as.data.frame(area_data1$nodes[2])
+
+head(node2)
+nrow(node2)
+nrow(node1)
 
 
-as_igraph(gschw)
+relation1 <- as.data.frame(area_data1$relations[1])
+relation2 <- as.data.frame(area_data1$relations[2])
+relation3 <- as.data.frame(area_data1$relations[3])
+
+head(relation3)
+
+ways1 <- as.data.frame(area_data1$ways[1])
+ways2 <- as.data.frame(area_data1$ways[2])
+ways3 <- as.data.frame(area_data1$ways[3])
+
+summary(area_data1$nodes)
+
+hw_ids <- find(area_data1, way(tags(k == "highway")))
+hw_ids <- find_down(area_data1, way(hw_ids))
+
+plot(area_data1, xlim = c(-122.8, -122.7))
+?plot
+plot_ways(hw, add = TRUE, col = "green")
+hw <- subset(area_data1, ids = hw_ids)
